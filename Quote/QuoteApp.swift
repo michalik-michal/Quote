f4a6643423b579239a835028a85955068e8cd47b
Michal Michalik <michalik515@gmail.com>
2022-12-11 07:35:38 +0100

    Initial Commit

---
+++ Quote/QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteApp.swift
+//  Quote
+//
+//  Created by Michał Michalik on 11/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
